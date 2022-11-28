2d62aed44da4826738c63c8fd1dafd72d66e9458
WeslBaker <89235616+WeslBaker@users.noreply.github.com>
2022-11-28 16:43:10 -0600

    Added boilerplate, started function

---
+++ svm.py
@@ -0,0 +1,21 @@
+#Import general
+import pandas as pd
+import numpy as np
+import math
+
+#Import SVM and data processing
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import _data
+from sklearn.svm import SVC
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+
+def normalize(historical_data):
+    df = historical_data
+    
+
+
+#Normalize data
+
+#Train SVM
+
+#Test SVM
