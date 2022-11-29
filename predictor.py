8cca0bbe31230e07f8d5146f58d1b415740ba855
WeslBaker <89235616+WeslBaker@users.noreply.github.com>
2022-11-28 20:54:28 -0600

    Filled out data processing

--- predictor.py
+++ predictor.py
@@ -1,3 +1,4 @@
+#Import 
 import pandas as pd
 import numpy as np
 import math
@@ -13,4 +14,45 @@ from sklearn.preprocessing import _data
 from sklearn.svm import SVC
 
 #Create dataframe
-df = 
+data = 'data/NCAAHistoricalData.csv'
+df = pd.read_csv(data)
+
+#Trim dataframe/change string values to int
+def format(data):
+    data.pop('index')
+    data.pop('Year')
+    data.pop('Team1')
+    data.pop('Team2')
+    data.pop("winner")
+    data.pop("Score1")
+    data.pop("Score2")
+    data.pop("G1")
+    data.pop("G2")
+
+    return data
+
+#Split data to train and test sets
+def split(data):
+    train_x, test_x, train_y, test_y= None,None,None,None
+    train_x, test_x, train_y, test_y = train_test_split( data.values[:4032, :47], data.values[:4032, 47], test_size = .2, train_size = .8, shuffle = False)
+    return (train_x, train_y, test_x, test_y)
+
+##Normalize the data
+def normalize(data):
+    scaler = MinMaxScaler()
+
+    scaler.fit(data)
+
+    data = scaler.transform(data)
+
+    return data, scaler
+
+#PCA/ICA
+
+#Retry both methods of training
+
+if __name__ == "__main__":
+    print(format(df))
+    train_x, train_y, test_x, test_y = split(df)
+    print(train_x)
+    print(test_y)
